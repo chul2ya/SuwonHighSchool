db2e9735f1ccad65d20dfa0d67e6c3324cf3e336
cft6746 <cft6746@gmail.com>
2020-02-10 11:27:47 +0900

    Added Basic Framework v1.0

---
+++ Framework/Object.h
@@ -0,0 +1,18 @@
+#pragma once
+class Object
+{
+public:
+	D3DXVECTOR2 position = D3DXVECTOR2(0.0f, 0.0f);
+	vector<D3DXVECTOR2> collisionLine;
+	string tag = "";
+
+	bool isRemove = false;
+
+	virtual void Update(float deltaTime) = 0;
+	virtual void OnCollisionInprogress(Object* another, float deltaTime) { };
+	virtual void LateUpdate(float deltaTime) = 0;
+	virtual void Render();
+	virtual void SetCollision(Sprite* spr);
+	virtual void SetCollisionAngle(D3DXVECTOR2 center, float angle);
+};
+
