977b4614abae7f7a2710f877904c929daae28d94
cft6746 <cft6746@gmail.com>
2020-02-12 13:11:17 +0900

    sprite

--- Framework/Sprite.cpp
+++ Framework/Sprite.cpp
@@ -7,6 +7,26 @@ Sprite::Sprite()
 {
 }
 
+Sprite::Sprite(string path, D3DXVECTOR2 imagesize)
+{
+	imageSize = imagesize;
+
+	_finddata_t fd;
+	intptr_t handle;
+	if ((handle = _findfirst(path.c_str(), &fd)) == -1L)
+	{
+
+		return;
+	}
+	path.erase(path.begin() + path.rfind("/") + 1, path.end());
+
+	do
+	{
+		ptexs.push_back(TextureManager::GetInstance()->GetTexture(path + fd.name));
+	} while (_findnext(handle, &fd) == 0);
+	_findclose(handle);
+}
+
 Sprite::Sprite(string path)
 {
 	_finddata_t fd;
@@ -32,7 +52,7 @@ Sprite::~Sprite()
 void Sprite::Update(float deltaTime)
 {
 	curAniTime += deltaTime;
-	if (curAniTime > aniTime) 
+	if (curAniTime > aniTime)
 	{
 		curAniTime -= aniTime;
 		if (++curScene >= (int)ptexs.size())
@@ -40,7 +60,7 @@ void Sprite::Update(float deltaTime)
 			if (loopAni)
 				curScene = 0;
 			else
-				curScene  -= 1;
+				curScene -= 1;
 		}
 	}
 }
@@ -55,17 +75,31 @@ void Sprite::Render()
 
 	auto pD3D9Device = Direct3D::GetInstance()->pD3D9Device;
 
-	Direct3D::Vertex vertex[4] = {
-		Direct3D::Vertex {D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(0.0f, 1.0f)},
-		Direct3D::Vertex {D3DXVECTOR3(0.0f, (float)pTex->Info.Height, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(0.0f, 0.0f)},
-		Direct3D::Vertex {D3DXVECTOR3((float)pTex->Info.Width, 0.0f, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(1.0f, 1.0f)},
-		Direct3D::Vertex {D3DXVECTOR3((float)pTex->Info.Width, (float)pTex->Info.Height, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(1.0f, 0.0f)},
-	};
+	Direct3D::Vertex vertex[4] = { };
+	if (imageSize.x == 0.0f && imageSize.y == 0.0f) //이미지 크기가 기본값이면
+	{
+		vertex[0] = { Direct3D::Vertex {D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(0.0f, 1.0f)} };
+		vertex[1] = { Direct3D::Vertex {D3DXVECTOR3(0.0f, (float)pTex->Info.Height, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(0.0f, 0.0f)} };
+		vertex[2] = { Direct3D::Vertex {D3DXVECTOR3((float)pTex->Info.Width, 0.0f, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(1.0f, 1.0f)} };
+		vertex[3] = { Direct3D::Vertex {D3DXVECTOR3((float)pTex->Info.Width, (float)pTex->Info.Height, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(1.0f, 0.0f)} };
+	}
+	else
+	{
+		D3DXVECTOR2 uvuxSize = D3DXVECTOR2(imageSize.x / pTex->Info.Width, imageSize.y / pTex->Info.Height);  //비율단위 이미지 크기
+		D3DXVECTOR2 uvuxPos = D3DXVECTOR2(imageScenePos.x / pTex->Info.Width, imageScenePos.y / pTex->Info.Height);  //비율단위 이미지 위치
+		vertex[0] = { Direct3D::Vertex {D3DXVECTOR3(0.0f, 0.0f, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(uvuxPos.x, uvuxPos.y + uvuxSize.y)} };
+		vertex[1] = { Direct3D::Vertex {D3DXVECTOR3(0.0f , imageSize.y, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(uvuxPos.x, uvuxPos.y)} };
+		vertex[2] = { Direct3D::Vertex {D3DXVECTOR3(imageSize.x, 0.0f, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(uvuxPos.x + uvuxSize.x, uvuxPos.y + uvuxSize.y)} };
+		vertex[3] = { Direct3D::Vertex {D3DXVECTOR3(imageSize.x, imageSize.y, 0.0f), D3DCOLOR_ARGB(a,r,g,b), D3DXVECTOR2(uvuxPos.x + uvuxSize.x, uvuxPos.y)} };
+	}
 
 	std::memcpy(ppVB, vertex, sizeof(vertex));
 	Direct3D::GetInstance()->pVB->Unlock();
-
-	D3DXVECTOR2 _tex = D3DXVECTOR2((float)pTex->Info.Width, (float)pTex->Info.Height);
+	D3DXVECTOR2 _tex;
+	if (imageSize.x == 0.0f && imageSize.y == 0.0f) //이미지 크기가 기본값이면
+		_tex = D3DXVECTOR2((float)pTex->Info.Width, (float)pTex->Info.Height);
+	else
+		_tex = D3DXVECTOR2(imageSize.x, imageSize.y);
 	D3DXVECTOR2 _center = D3DXVECTOR2(center.x * _tex.x, center.y * _tex.y);
 	D3DXVECTOR2 _scaleAnchor = D3DXVECTOR2(scaleAnchor.x * _tex.x, scaleAnchor.y * _tex.y) + _center;
 	D3DXVECTOR2 _rotateAnchor = D3DXVECTOR2(rotateAnchor.x * _tex.x, rotateAnchor.y * _tex.y) + _center;
@@ -76,7 +110,7 @@ void Sprite::Render()
 
 	pD3D9Device->SetTransform(D3DTS_WORLD, &matrix);
 
-	pD3D9Device->SetTexture(0, pTex->pImage);	
+	pD3D9Device->SetTexture(0, pTex->pImage);
 
 	pD3D9Device->SetStreamSource(0, Direct3D::GetInstance()->pVB, 0, sizeof(Direct3D::Vertex));
 	pD3D9Device->SetFVF(Direct3D::GetInstance()->VertexFVF);
--- Framework/Sprite.h
+++ Framework/Sprite.h
@@ -3,8 +3,6 @@
 class Sprite
 {
 public:
-
-	// 이 코드는 이러한 뜻을 가지고 설계되었습니다.
 	vector<Texture*> ptexs;
 	DWORD color = 0xffffffff;
 	int a = 255, r = 255, g = 255, b = 255;
@@ -14,6 +12,8 @@ public:
 	float curAniTime = 0.0f;
 	bool loopAni = true;
 
+	D3DXVECTOR2 imageSize = D3DXVECTOR2(0.0f, 0.0f); //이미지 크기  0, 0이면 기본값  픽셀단위
+	D3DXVECTOR2 imageScenePos = D3DXVECTOR2(0.0f, 0.0f); //이미지 장면 위치  왼쪽 위 기준  픽셀단위
 	D3DXVECTOR2 pos = D3DXVECTOR2(0.0f, 0.0f);
 	D3DXVECTOR2 scale = D3DXVECTOR2(1.0f, 1.0f);
 	D3DXVECTOR2 scaleAnchor = D3DXVECTOR2(0.0f, 0.0f);
@@ -22,7 +22,8 @@ public:
 	D3DXVECTOR2 center = D3DXVECTOR2(0.5f, 0.5f);
 
 	Sprite();
-	Sprite(string pathWithName);
+	Sprite(string path, D3DXVECTOR2 imagesize);
+	Sprite(string path);
 	virtual ~Sprite();
 
 	void Update(float deltaTime);
